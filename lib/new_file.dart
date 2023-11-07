701e3035a5be260ac7cea661099ecdd1c0f567bf
ahmad-nour-h <ahmadnourhaedr@gmail.com>
2023-11-07 08:16:45 +0200

    title

--- lib/new_file.dart
+++ lib/new_file.dart
@@ -6,7 +6,9 @@ class NewFile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('title'),
+      ),
     );
   }
 }
